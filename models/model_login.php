05d0ceaeb638f673d41baffe27cf740e68bdb631
Maksim Mazein <maksim@mazein.ru>
2021-03-10 23:15:02 +0500

    commit 2

--- models/model_login.php
+++ models/model_login.php
@@ -6,6 +6,30 @@ Class Model_Login{
 	public function login($data){
 		global $db;
 		$this->db = $db;
+		
+		//Создание таблиц в БД
+			$sql  = "CREATE TABLE IF NOT EXISTS `tasks` ( ";
+			$sql .= "`task_id` int(11) NOT NULL AUTO_INCREMENT, ";
+			$sql .= "`user_id` int(11) NOT NULL, ";
+			$sql .= "`text` text NOT NULL, ";
+			$sql .= "`date_added` datetime NOT NULL, ";
+			$sql .= "PRIMARY KEY (`task_id`) ";
+			$sql .= ") ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+			
+			$this->db->query($sql);
+			
+			$sql  = "CREATE TABLE IF NOT EXISTS `users` ( ";
+			$sql .= "`id` int(11) NOT NULL AUTO_INCREMENT, ";
+			$sql .= "`login` varchar(32) NOT NULL, ";
+			$sql .= "`firstname` varchar(32) NOT NULL, ";
+			$sql .= "`lastname` varchar(32) NOT NULL, ";
+			$sql .= "`email` varchar(32) NOT NULL, ";
+			$sql .= "`password` varchar(128) NOT NULL, ";
+			$sql .= "PRIMARY KEY (`id`) ";
+			$sql .= ") ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+			
+			$this->db->query($sql);
+		//Создание таблиц в БД
 
 		$query = $this->db->query("SELECT * FROM users WHERE email = '" . $this->db->escape($data['email']) . "' AND password = '" . $this->db->escape(md5($data['password'])) . "'");
 		
